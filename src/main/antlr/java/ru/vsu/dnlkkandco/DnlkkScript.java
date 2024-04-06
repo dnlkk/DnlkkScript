f5ea5dae46f4cb2d46aa806a169513c9e437f74c
Putin Pavel Alexandrovich <pavelputin2003@yandex.ru>
2024-04-06 10:30:29 +0300

    refact: made class TreeViewAst

--- src/main/antlr/java/ru/vsu/dnlkkandco/DnlkkScript.java
+++ src/main/antlr/java/ru/vsu/dnlkkandco/DnlkkScript.java
@@ -23,28 +23,10 @@ public class DnlkkScript {
         ParseTree tree = parser.program();
         try {
             AstNode node = tree.accept(new AstBuilderVisitor());
-            String result = treeViewAst(node);
-            System.out.println(result);
+            System.out.println(new TreeViewAst(node));
         } catch (Exception e) {
             System.err.println("Parsing error");
             System.err.println(e.getMessage());
         }
-    }
-
-    static String treeViewAst(AstNode tree) {
-        return treeViewAstInner(tree, new StringBuffer(), "", "", "").toString();
-    }
-
-    static StringBuffer treeViewAstInner(AstNode tree, StringBuffer out, String prevPrefix, String prefixLine, String prefixNode) {
-        String line = prevPrefix + prefixNode + (tree == null ? "null" : tree.getName());
-
-        out.append(line).append('\n');
-        for (int i = 0; tree != null && i < tree.getChildrenAmount(); i++) {
-            boolean last = i == tree.getChildrenAmount() - 1;
-            String nextLevelPrefix = last ? "  " : "│ ";
-            String nodePrefix = last ? "└─" : "├─";
-            treeViewAstInner(tree.getChild(i), out, prevPrefix + prefixLine, nextLevelPrefix, nodePrefix);
-        }
-        return out;
     }
 }
---
+++ src/main/antlr/java/ru/vsu/dnlkkandco/TreeViewAst.java
@@ -0,0 +1,34 @@
+package ru.vsu.dnlkkandco;
+
+import java.util.stream.IntStream;
+
+public class TreeViewAst {
+    private final AstNode tree;
+
+    public TreeViewAst(AstNode tree) {
+        this.tree = tree;
+    }
+
+    @Override
+    public String toString() {
+        return treeViewAstInner(tree, new StringBuffer(), "", "", "").toString();
+    }
+
+    private StringBuffer treeViewAstInner(AstNode tree, StringBuffer out, String prevPrefix, String prefixLine, String prefixNode) {
+        String line = prevPrefix + prefixNode + (tree == null ? "null" : tree.getName());
+
+        out.append(line).append('\n');
+
+        if (tree != null) {
+            IntStream.range(0, tree.getChildrenAmount())
+                    .forEach(i -> {
+                        boolean last = i == tree.getChildrenAmount() - 1;
+                        String nextLevelPrefix = last ? "  " : "│ ";
+                        String nodePrefix = last ? "└─" : "├─";
+                        treeViewAstInner(tree.getChild(i), out, prevPrefix + prefixLine, nextLevelPrefix, nodePrefix);
+                    });
+        }
+
+        return out;
+    }
+}
