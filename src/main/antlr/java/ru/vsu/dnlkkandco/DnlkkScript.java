23c93187700d10f76215312d0d2de14983fa8999
Evgeniy Sakov <91192469+PiPuKaPRo@users.noreply.github.com>
2024-07-02 22:25:33 +0300

    соединение воедино

--- src/main/antlr/java/ru/vsu/dnlkkandco/DnlkkScript.java
+++ src/main/antlr/java/ru/vsu/dnlkkandco/DnlkkScript.java
@@ -5,12 +5,14 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import ru.vsu.dnlkkandco.gen.DnlkkRulesLexer;
 import ru.vsu.dnlkkandco.gen.DnlkkRulesParser;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class DnlkkScript {
     public static void main(String[] args) throws IOException {
         FileInputStream inputStream = new FileInputStream("src/main/resources/testProgram");
+        String outputFilePath = "C:\\Users\\Евген\\Desktop\\DnlkkScript-dev\\src\\main\\resources\\output.txt";
 
         CharStream input = CharStreams.fromStream(inputStream);
 
@@ -20,16 +22,42 @@ public class DnlkkScript {
 
         DnlkkRulesParser parser = new DnlkkRulesParser(tokens);
 
+        CodeGeneration codeGeneration = new CodeGeneration();
+
         ParseTree tree = parser.program();
         try {
             AstNode node = tree.accept(new AstBuilderVisitor());
+            System.out.println(node.toString());
             String result = treeViewAst(node);
             System.out.println(result);
+            codeGeneration.writeToFile("C:\\Users\\Евген\\Desktop\\DnlkkScript-dev\\src\\main\\resources\\output.txt", node);
+            runInterpreter(outputFilePath);
         } catch (Exception e) {
             System.err.println("Parsing error");
             System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void runInterpreter(String outputFilePath) {
+        try {
+            ProcessBuilder pb = new ProcessBuilder(
+                    "java",
+                    "-cp",
+                    "C:\\Users\\Евген\\Desktop\\DnlkkScript-dev\\interpreter\\src\\main\\java\\ru\\vsu\\dnlkkandco\\Interpreter.java",
+                    "C:\\Users\\Евген\\Desktop\\DnlkkScript-dev\\interpreter\\src\\main\\java\\ru\\vsu\\dnlkkandco\\Interpreter.java",
+                    outputFilePath
+            );
+            pb.inheritIO();
+            Process process = pb.start();
+            process.waitFor();
+        } catch (Exception e) {
+            System.err.println("Error running interpreter:");
+            e.printStackTrace();
         }
     }
+
 
     static String treeViewAst(AstNode tree) {
         return treeViewAstInner(tree, new StringBuffer(), "", "", "").toString();
