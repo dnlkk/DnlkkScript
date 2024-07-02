5b1f5a4f9da22693440f3b0192e01ac38113884e
Evgeniy Sakov <91192469+PiPuKaPRo@users.noreply.github.com>
2024-07-02 16:58:51 +0300

    сделаны функции

--- src/main/antlr/java/ru/vsu/dnlkkandco/CodeGeneration.java
+++ src/main/antlr/java/ru/vsu/dnlkkandco/CodeGeneration.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 public class CodeGeneration {
 
     private final StringBuilder output = new StringBuilder();
+    private final StringBuilder function = new StringBuilder();
     private int counterForIf;
     private int counterForFor;
     private int counterForWhile;
@@ -15,6 +16,7 @@ public class CodeGeneration {
         output.setLength(0);
         output.append("#main\n");
         processNode(node);
+        output.append(function);
         return output.toString();
     }
 
@@ -49,15 +51,15 @@ public class CodeGeneration {
     private void processTerminal(TerminalAstNode node) {
         String name = node.getName();
         if (isNumeric(name)) {
-            output.append("PUSH N").append(name).append("\n");
+            output.append("push N").append(name).append("\n");
         } else if (isDouble(name)) {
-            output.append("PUSH D").append(name).append("\n");
+            output.append("push D").append(name).append("\n");
         } else if (isBoolean(name)) {
-            output.append("PUSH B").append(name).append("\n");
+            output.append("push B").append(name).append("\n");
         } else if (isStringLiteral(name)) {
-            output.append("PUSH \"").append(name).append("\"\n");
+            output.append("push \"").append(name).append("\"\n");
         } else {
-            output.append("PUSH \"").append(name).append("\"\n").append("LOAD\n");
+            output.append("push \"").append(name).append("\"\n").append("load\n");
         }
     }
 
@@ -78,22 +80,22 @@ public class CodeGeneration {
     private void processBinOp(BinOpNode node) {
         if (node.getName().equals("var")){
             processNode(node.getChild(1));
-            output.append("PUSH \"").append(node.getChild(0).getName()).append("\"\n").append("SET\n");
+            output.append("push \"").append(node.getChild(0).getName()).append("\"\n").append("SET\n");
         } else {
             processNode(node.getChild(0));
             processNode(node.getChild(1));
             String operator = switch (node.getName()) {
-                case "+" -> "ADD";
-                case "-" -> "SUB";
-                case "*" -> "MUL";
-                case "/" -> "DIV";
-                case "%" -> "MOD";
-                case "==" -> "EQ";
-                case "!=" -> "NEQ";
-                case ">" -> "GT";
-                case ">=" -> "GTE";
-                case "<" -> "LT";
-                case "<=" -> "LTE";
+                case "+" -> "add";
+                case "-" -> "sub";
+                case "*" -> "mul";
+                case "/" -> "div";
+                case "%" -> "mod";
+                case "==" -> "eq";
+                case "!=" -> "neq";
+                case ">" -> "gt";
+                case ">=" -> "gte";
+                case "<" -> "lt";
+                case "<=" -> "lte";
                 default -> throw new RuntimeException("Unknown binop");
             };
             output.append(operator).append("\n");
@@ -104,8 +106,8 @@ public class CodeGeneration {
     private void processUnaryOp(UnaryOpNode node) {
         processNode(node.getChild(0));
         String operator = switch (node.getName()) {
-            case "-" -> "NEG";
-            case "!" -> "NOT";
+            case "-" -> "neg";
+            case "!" -> "not";
             default -> throw new RuntimeException("Unknown unop");
         };
         output.append(operator).append("\n");
@@ -113,85 +115,92 @@ public class CodeGeneration {
 
     private void processObjectCall(ObjectCallNode node) {
         processNode(node.getChild(1));
-        output.append("PUSH \"").append(node.getChild(0).getName()).append("\"\n").append("LOAD\n")
-                .append("GETFIELD\n");
+        output.append("push \"").append(node.getChild(0).getName()).append("\"\n").append("load\n")
+                .append("getfield\n");
     }
 
     private void processArrayCall(ArrayCallNode node) {
         processNode(node.getChild(0));
         processNode(node.getChild(1));
-        output.append("ALOAD\n");
+        output.append("aload\n");
     }
 
     private void processFunctionCall(FunctionCallNode node) {
-        processNode(node.getChild(0));
+        int index = output.indexOf("#main\n");
+        String insert = "push \"#" + node.getChild(0).getName() + "\"\n" + "newfunc\n" + "push \""
+                + node.getChild(0).getName() + "\"\n" + "set\n";
+        output.insert(index + "#main\n".length(), insert);
+        output.append("newarray\n");
         for (int i = 1; i < node.getChildrenAmount(); i++) {
+            output.append("dup\n");
+            output.append("push N").append(i - 1).append("\n");
             processNode(node.getChild(i));
+            output.append("aset\n");
         }
-        output.append("CALLFUNC\n");
+        processNode(node.getChild(0));
+        output.append("callfunc\n");
     }
 
     private void processFunctionDefinition(FunctionDefinitionNode node) {
-        output.append("NEWFUNC ").append(node.getName()).append(" ")
-                .append(node.getChildrenAmount() - 2).append("\n");
-        for (int i = 1; i < node.getChildrenAmount() - 1; i++) {
-            output.append(node.getChild(i).getName()).append(" ");
-        }
+        String functionName = node.getChild(0).getName();
         output.append("\n");
-        processNode(node.getChild(node.getChildrenAmount() - 1));
+        function.append("#").append(functionName).append("\n");
+        for (int i = 1; i < node.getChildrenAmount() - 1; i++) {
+            function.append("push \"__args__\"\nload\n");
+            function.append("push N").append(i - 1).append("\naload\n");
+            function.append("push \"").append(node.getChild(i).getName()).append("\"\nset\n");
+        }
+        function.append(node.getChild(node.getChildrenAmount() - 1));
     }
 
     private void processObjectLiteral(ObjectLiteralNode node) {
-        output.append("NEWOBJECT\n");
+        output.append("newobject\n");
         for (AstNode child : node.children) {
             processNode(child);
         }
     }
 
     private void processArrayLiteral(ArrayLiteralNode node) {
-        output.append("NEWARRAY\n");
+        output.append("newarray\n");
         for (int i = 0; i < node.getChildrenAmount(); i++) {
-            output.append("DUP\n");
+            output.append("dup\n");
             processNode(node.getChild(i));
-            output.append("PUSH" + " N" + i + "\n");
-            output.append("ASET\n");
+            output.append("push" + " N" + i + "\n");
+            output.append("aset\n");
         }
     }
 
     private void processField(FieldNode node) {
         processNode(node.getChild(1));
         processNode(node.getChild(0));
-        output.append("LOAD\n").append("SETFIELD").append(node.getChild(0).getName()).append("\n");
+        output.append("load\n").append("setfield").append(node.getChild(0).getName()).append("\n");
     }
 
     private void processReturn(ReturnNode node) {
         processNode(node.getChild(0));
-        output.append("RETURN\n");
+        output.append("return\n");
     }
 
     private void processAssign(AssignNode node) {
         if (node.getChild(0) instanceof ObjectCallNode) {
             processNode(node.getChild(1));
-            output.append("PUSH \"").append(node.getChild(0).getChild(1).getName()).append("\"\n");
-            output.append("PUSH \"").append(node.getChild(0).getChild(0).getName()).append("\"\n").append("LOAD\n")
-                    .append("SETFIELD\n");
+            output.append("push \"").append(node.getChild(0).getChild(1).getName()).append("\"\n");
+            output.append("push \"").append(node.getChild(0).getChild(0).getName()).append("\"\n").append("load\n")
+                    .append("setfield\n");
         } else if (node.getChild(0) instanceof ArrayCallNode) {
-            output.append("PUSH \"").append(node.getChild(0).getChild(0).getName()).append("\"\n").append("LOAD\n")
-                    .append("PUSH \"").append(node.getChild(0).getChild(1).getName()).append("\"\n");
+            output.append("push \"").append(node.getChild(0).getChild(0).getName()).append("\"\n").append("load\n")
+                    .append("push \"").append(node.getChild(0).getChild(1).getName()).append("\"\n");
             processNode(node.getChild(1));
-            output.append("ASET\n");
+            output.append("aset\n");
         }
         processNode(node.getChild(1));
-        output.append("PUSH \"").append(node.getChild(0).getName()).append("\"\n").append("SET\n");
+        output.append("push \"").append(node.getChild(0).getName()).append("\"\n").append("set\n");
     }
 
     private void processWhile(WhileNode node) {
-        output.append("#start_while").append(counterForWhile).append("\n");
         processNode(node.getChild(0));
-        output.append("JMF #end_while").append(counterForWhile).append("\n");
-        ;
+        output.append("jmf end_while").append(counterForWhile).append("\n");
         processNode(node.getChild(1));
-        output.append("JMP #start_while").append(counterForWhile).append("\n").append("#end_while").append(counterForWhile).append("\n");
         counterForWhile++;
     }
 
@@ -203,7 +212,7 @@ public class CodeGeneration {
         int elif = counterForElif;
         counterForElif++;
         processNode(node.getChild(0));
-        output.append("JMF elif_end").append(elif).append("\n");
+        output.append("jmf elif_end").append(elif).append("\n");
         processNode(node.getChild(1));
         output.append("#elif_end").append(elif).append("\n");
     }
@@ -212,7 +221,7 @@ public class CodeGeneration {
         int endIf = counterForIf;
         counterForIf++;
         processNode(node.getChild(0));
-        output.append("JMF end_if").append(endIf).append("\n");
+        output.append("jmf end_if").append(endIf).append("\n");
         processNode(node.getChild(1));
         output.append("#end_if").append(endIf).append("\n");
         for (int i = 2; i < node.getChildrenAmount() - 1; i++) {
@@ -225,10 +234,10 @@ public class CodeGeneration {
         processNode(node.getChild(0));
         output.append("#for").append(counterForFor).append("\n");
         processNode(node.getChild(1));
-        output.append("JMF end_for").append(counterForFor).append("\n");
+        output.append("jmf end_for").append(counterForFor).append("\n");
         processNode(node.getChild(3));
         processNode(node.getChild(2));
-        output.append("JMP for").append(counterForFor).append("\n");
+        output.append("jmp for").append(counterForFor).append("\n");
         output.append("#end_for").append(counterForFor).append("\n");
         counterForFor++;
     }
@@ -266,74 +275,3 @@ public class CodeGeneration {
         }
     }
 }
-
-//#main
-//        NEWARRAY
-//dup
-//PUSH N1
-//PUSH N0
-//ASET
-//        DUP
-//PUSH N2
-//PUSH N1
-//ASET
-//        DUP
-//PUSH N3
-//PUSH N2
-//ASET
-//PUSH "a"
-//SET
-//for
-//PUSH "a"
-//LOAD
-//PUSH N0
-//ALOAD
-//PUSH N2
-//LT
-//JMF end_for
-//#start_for
-//PUSH N3
-//PUSH N5
-//PUSH N2
-//ADD
-//        MUL
-//PUSH N3
-//PUSH N1
-//ADD
-//        ADD
-//PUSH "c"
-//SET
-//        NEWARRAY
-//dup
-//PUSH N0
-//PUSH "a"
-//LOAD
-//        ASET
-//dup
-//PUSH N1
-//PUSH "b"
-//LOAD
-//        ASET
-//PUSH "c"
-//LOAD
-//PUSH N2
-//ALOAD
-//PUSH "foo"
-//GETFIELD
-//        CALLFUNC
-//PUSH N0
-//ALOAD
-//        RETURN
-//PUSH "a"
-//LOAD
-//PUSH "a"
-//LOAD
-//PUSH N0
-//ALOAD
-//PUSH N2
-//ADD
-//PUSH N0
-//ASET
-//JMP #start_for
-//#end_for
-//        HALT
